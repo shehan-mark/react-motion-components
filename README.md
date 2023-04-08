3a1e49d870b4157c40f72b31346003cebb3f9004
Shehan Neomal Mark Fonseka <markfonseka.z@gmail.com>
2023-04-08 16:09:36 +0530

    component library builds with styles and can test using storybook

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # react-motion-components
 Motion prioritized react component library
+
+
+## Resources to start and maintain a Component library
+
+### Main guide that I used
+// https://dev.to/alexeagleson/how-to-create-and-publish-a-react-component-library-2oe
+
+### Testing our lib locally first
+https://javascript.plainenglish.io/testing-a-local-react-npm-package-with-ease-7d0668676ddb
--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,9 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
 
 import packageJson from './package.json' assert { type: "json" };
 
-// https://dev.to/alexeagleson/how-to-create-and-publish-a-react-component-library-2oe
 export default [
   {
     input: "src/index.ts",
@@ -24,12 +22,10 @@ export default [
     ],
     plugins: [
       resolve(),
-      postcss({
-        use: ['sass'],
-      }),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
     ],
+    external: ["react", "react-dom"]
   },
   {
     input: "dist/esm/types/index.d.ts",
