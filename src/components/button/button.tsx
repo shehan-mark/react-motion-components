bd27bf7e7fa106ede56b62ff618dd133165d389b
Shehan Neomal Mark Fonseka <markfonseka.z@gmail.com>
2023-04-24 15:03:18 +0530

    lint fixes

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import { Button as StyledButton } from "./styled";
+import { Button as StyledButton } from './styled';
 
 export enum ButtonType {
   DEFAULT,
