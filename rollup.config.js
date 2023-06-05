b73e1ed3754df465780486b942d2f740bd028650
Shehan Neomal Mark Fonseka <markfonseka.z@gmail.com>
2023-06-05 22:04:51 +0530

    clean and re-add essential packages to build and optimize the build

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import terser from '@rollup/plugin-terser';
 
 import packageJson from './package.json' assert { type: "json" };
 
@@ -24,6 +25,7 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      terser()
     ],
     external: ["react", "react-dom"]
   },
@@ -31,6 +33,6 @@ export default [
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.scss$/]
+    external: [/\.(css|sass|scss)$/], // asking not to process these types of files
   },
 ];
