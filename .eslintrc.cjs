0088d2e033ecdca23ebfc92a7c77a76049d516ce
Shehan Neomal Mark Fonseka <markfonseka.z@gmail.com>
2023-06-06 22:03:33 +0530

    enabling storybook for visual debugging

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,51 +1,38 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'jest': true,
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/typescript',
-        'airbnb',
-        'airbnb-typescript',
-    ],
-    'overrides': [
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module',
-        'project': './tsconfig.json'
-    },
-    'plugins': [
-        'react',
-        '@typescript-eslint',
-        'prettier'
-    ],
-    'rules': {
-        'indent': [
-            'error',
-            2
-        ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
-        "react/require-default-props": [
-            "error", 
-            {
-              "forbidDefaultForRequired": false,        
-              "functions": "defaultArguments" //Here
-            }
-        ],
-        "import/prefer-default-export": "off",
-        "import/no-extraneous-dependencies": ["error", {"devDependencies": true}]
-    }
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'jest': true
+  },
+  'extends': [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:storybook/recommended"
+  ],
+  'overrides': [],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+    'project': './tsconfig.json'
+  },
+  'plugins': ['react', '@typescript-eslint', 'prettier'],
+  'rules': {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'semi': ['error', 'always'],
+    "react/require-default-props": ["error", {
+      "forbidDefaultForRequired": false,
+      "functions": "defaultArguments" //Here
+    }],
+
+    "import/prefer-default-export": "off",
+    "import/no-extraneous-dependencies": ["error", {
+      "devDependencies": true
+    }]
+  }
 };
--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,22 +1,22 @@
-// import React from 'react';
-// // eslint-disable-next-line import/no-extraneous-dependencies
-// import { ComponentStory, ComponentMeta } from '@storybook/react';
-// import Button from './button';
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Button from './button';
 
-// // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-// export default {
-//   title: 'My/Button',
-//   component: Button,
-// } as ComponentMeta<typeof Button>;
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
 
-// // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-// // eslint-disable-next-line react/jsx-props-no-spreading, react/function-component-definition
-// const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// eslint-disable-next-line react/jsx-props-no-spreading, react/function-component-definition
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-// export const ButtonOne = Template.bind({});
-// // More on args: https://storybook.js.org/docs/react/writing-stories/args
-// ButtonOne.args = {
-//   // eslint-disable-next-line no-console
-//   action: () => console.log('clicked me'),
-//   buttonText: 'Storybook Button',
-// };
+export const ButtonOne = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+ButtonOne.args = {
+  // eslint-disable-next-line no-console
+  action: () => console.log('clicked me'),
+  buttonText: 'Storybook Button',
+};
