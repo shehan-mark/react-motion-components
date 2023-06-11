3c90ceb85d587eeb1c5259374727e65420e055bd
shehan <markfonseka.z@gmail.com>
2023-06-11 15:42:32 +0530

    refactoring writing tests

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -4,13 +4,7 @@ import { render } from '@testing-library/react';
 import Button from './button';
 
 describe('Button', () => {
-  // test('renders the Button component', () => {
-  //   render(<Button buttonText="Test Button" action={() => console.log('clicked')} />);
-  // });
-  const renderComponent = () => render(<Button buttonText="Test Button" action={() => {}} />);
-  it('should have something', () => {
-    const { findByRole } = renderComponent();
-    const buttonComp = findByRole('button');
-    expect(buttonComp).toHaveAttribute('type', 'button');
+  test('renders the Button component', () => {
+    render(<Button buttonText="Test Button" action={() => {}} />);
   });
 });
