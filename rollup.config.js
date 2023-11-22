6b9df48de3bd626c423cd14ee4ccf5f004ee4efa
Shehan Neomal Mark Fonseka <markfonseka.z@gmail.com>
2023-11-22 23:42:25 +0530

    initial try

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,10 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import terser from '@rollup/plugin-terser';
+import scss from "rollup-plugin-postcss";
+import postcss from "rollup-plugin-scss";
+
+import { visualizer } from "rollup-plugin-visualizer";
 
 import packageJson from './package.json' assert { type: "json" };
 
@@ -25,14 +29,56 @@ export default [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      terser()
+      terser(),
+      visualizer({
+        filename: 'bundle-analysis.html',
+        open: true,
+      }),
     ],
     external: ["react", "react-dom"]
   },
   {
-    input: "dist/esm/types/index.d.ts",
+    input: "dist/esm/types/src/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
     external: [/\.(css|sass|scss)$/], // asking not to process these types of files
   },
+  {
+    input: 'styles/r-r-gui.scss',
+    plugins: scss({
+      output: packageJson.styles,
+    }),
+  },
 ];
+
+const extensions = ['.js', '.ts', '.jsx', '.tsx'];
+
+
+// export default {
+//   input: [
+//     './src/index.ts',
+//   ],
+//   output: {
+//     dir: 'dist',
+//     format: 'esm',
+//     preserveModules: true,
+//     preserveModulesRoot: 'src',
+//     sourcemap: true,
+//   },
+//   plugins: [
+//     resolve(),
+//     commonjs(),
+//     typescript({
+//       tsconfig: './tsconfig.json',
+//       declaration: true,
+//       declarationDir: 'dist',
+//     }),
+//     postcss(),
+//     terser(),
+    // visualizer({
+    //   filename: 'bundle-analysis.html',
+    //   open: true,
+    // }),
+//   ],
+//   external: ['react', 'react-dom'],
+// };
